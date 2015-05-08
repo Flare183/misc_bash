0d94ef0cada900f64c75f24f0b41a01f64406755
Flare183 <flare183@nctv.com>
2015-05-07 23:42:55 -0400

    Modified the Spinner Script, making it inf.! Yay!

--- spinner.sh
+++ spinner.sh
@@ -1,8 +1,15 @@
 #! /bin/bash
-sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
-sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
-sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
-sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
-sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
-sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
-sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
+# sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
+# sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
+# sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
+# sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
+# sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
+# sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
+# sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
+
+
+
+while true
+do
+    sleep 0.5; printf "\b/"; sleep 0.1; printf "\b-"; sleep 0.5; printf "\b\\"; sleep 0.5; printf "\b|"
+done

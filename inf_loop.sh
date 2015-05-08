0c33ae22c4eeb184cbf412b4f31157918250c2fe
Flare183 <flare183@nctv.com>
2015-05-07 22:38:53 -0400

    Fixed the inf. loop script. Wow I'm stupid.

--- inf_loop.sh
+++ inf_loop.sh
@@ -4,4 +4,13 @@
 # Bash script for inf. looping
 # Written by Jesse N. Richardson (flare183@charter.net) [negativeflare]
 
-
+while true
+do
+    echo "derp"
+    x=22
+    if [ !x == "22" ]
+	echo "Exiting"
+	then
+	break
+fi
+done

895fd6cb6c6bf41939994198013aac4d027a44d9
Flare183 <flare183@nctv.com>
2017-05-07 23:24:37 -0400

    Started working on a server sensors script

---
+++ server_sensors_watch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+while true
+do
+
+
+load1m=$(uptime | awk '{ print $10 }' | cut -c1-4)
+load10m=$(uptime | awk '{ print $11 }' | cut -c1-4)
+load15m=$(uptime | awk '{ print $12 }' | cut -c1-4)
+threshold1m="1.00"
+threshold10m="3.00"
+threshold15m="3.80"
+result1m=$(echo "$load1m > $threshold1m" | bc)
+result10m=$(echo "$load10m > $threshold10m" | bc)
+result15m=$(echo "$load15m > $threshold15m" | bc)
+
+if [ "$result1m" == 1 ]; then
+  pushover -t "Quartz: LoadAvg Warning" Quartz: LoadAvg 1m: '$result1m'
+elif [ "$result10m" == 1 ]; then
+  msg="Load 10 min: $load10m (threshold $threshold10m)"
+  subject="ALERT: High load 10m ($load10m)"
+  send=1
+elif [ "$result15m" == 1 ]; then
+  msg="Load 1 min: $load1m (threshold $threshold1m)"
+  subject="ALERT: High load 1m ($load1m)"
+  send=1
+fi
+done
+
+

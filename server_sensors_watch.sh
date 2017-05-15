d66bbce1b9d498e5c13fa8c11a1361787a061a17
Jesse N. Richardson <flare183@nctv.com>
2017-05-15 01:32:15 -0400

    Finally got to working on main loop commands.

--- server_sensors_watch.sh
+++ server_sensors_watch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# Reference: sensors | grep "^temp1" | grep -e '+.*C' | cut -f 2 -d '+' | cut -f 1 -d ' ' | sed 's/°C//'xs
+# Reference: sensors | grep "^temp1" | grep -e '+.*C' | cut -f 2 -d '+' | cut -f 1 -d ' ' | sed 's/°C//'
 while true
 do
 
-
-temp=$(sensors -u | grep -m 1 temp1_input)
+temp_core1=$(sensors | grep "^temp1" | grep -e '+.*C' | cut -f 2 -d '+' | cut -f 1 -d ' ' | sed 's/°C//')
+temp_core2=$(sensors | grep "^temp2" | grep -e '+.*C' | cut -f 2 -d '+' | cut -f 1 -d ' ' | sed 's/°C//')
 # load10m=$(uptime | awk '{ print $11 }' | cut -c1-4)
 # load15m=$(uptime | awk '{ print $12 }' | cut -c1-4)
 # threshold1m="1.00"
@@ -14,16 +14,16 @@ temp=$(sensors -u | grep -m 1 temp1_input)
 # result10m=$(echo "$load10m > $threshold10m" | bc)
 # result15m=$(echo "$load15m > $threshold15m" | bc)
 
-if [ "$temp" == 50 ]; then
-  pushover -t "Quartz: LoadAvg Warning" Quartz: LoadAvg 1m: '$result1m'
-elif [ "$result10m" == 1 ]; then
-  msg="Load 10 min: $load10m (threshold $threshold10m)"
-  subject="ALERT: High load 10m ($load10m)"
-  send=1
-elif [ "$result15m" == 1 ]; then
-  msg="Load 1 min: $load1m (threshold $threshold1m)"
-  subject="ALERT: High load 1m ($load1m)"
-  send=1
+if [ "$temp_core1" => 50.00 ]; then
+    pushover -t "Ruby Heat Sensors" Core 1: 'temp_core1'
+    sleep 120
+elif [ "$temp_core2" => 50.00 ]; then
+    pushover -t "Ruby Heat Sensors" Core 2: 'temp_core2'
+    sleep 120
+# elif [ "$result15m" == 1 ]; then
+#   msg="Load 1 min: $load1m (threshold $threshold1m)"
+#   subject="ALERT: High load 1m ($load1m)"
+#   send=1
 fi
 done
 

dd08fb59a42d1fd8ace184a64e5e79f83add8dc7
Jesse Richardson <flare183@charter.net>
2017-06-04 11:42:39 -0400

    Updated Quartz Sensors Script Peak Temp

--- server_sensors_quartz.sh
+++ server_sensors_quartz.sh
@@ -6,12 +6,12 @@ do
 
 temp_core1=$(sensors | grep "^temp1" | grep -e '+.*C' | cut -f 2 -d '+' | cut -f 1 -d ' ' | sed 's/°C//' && sleep 120)
 temp_core2=$(sensors | grep "^temp2" | grep -e '+.*C' | cut -f 2 -d '+' | cut -f 1 -d ' ' | sed 's/°C//' && sleep 120)
-peak_temp="40"
+peak_temp="45"
 
 if [[ "$temp_core1" > "$peak_temp" || "temp_core1" == "$peak_temp" ]]; then
-    pushover -t "Quartz Heat Sensors" Core 1 Is Over 40C!
+    pushover -t "Quartz Heat Sensors" Core 1 Is Over 45C!
 elif [[ "$temp_core2" > "$peak_temp" || "temp_core2" == "$peak_temp" ]]; then
-    pushover -t "Quartz Heat Sensors" Core 2 Is Over 40C!
+    pushover -t "Quartz Heat Sensors" Core 2 Is Over 45C!
 fi
 done
 

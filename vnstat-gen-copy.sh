cb2e47e930e32e9c795885143127f7898c226d19
Jesse Richardson <flare183@charter.net>
2018-02-15 15:26:46 -0500

    Fixed Vnstat script for desktop

--- vnstat-gen-copy.sh
+++ vnstat-gen-copy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Vnstat Graph Generator and copying script
 
-/usr/bin/vnstati -vs -i eth0 -o /tmp/vnstat-desktop.png
+/usr/bin/vnstati -vs -i enp2s0 -o /tmp/vnstat-desktop.png
 scp /tmp/vnstat-desktop.png jesse@quartz:/var/www/vnstat/

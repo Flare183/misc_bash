9cc541aaa8b7d8dc82ffee3659ac51c285926296
Jesse Richardson <flare183@charter.net>
2019-03-28 15:30:02 -0400

    Added Size Checking

--- minecraft_backup.sh
+++ minecraft_backup.sh
@@ -9,9 +9,11 @@ echo "Then 7zips it up and puts it in the backups folder"
 echo "NOTE: For this script to run properly you need the 7zip script in my misc_bash repo on Github"
 echo "https://github.com/Flare183/misc_bash"
 
+
 sleep 5
 
 tmux send-keys -t Minecraft save-all C-m
+sleep 10
 sync
 cp -Rv /home/minecraft/papernut /home/minecraft/papernut_"$(date +%F)"
 echo "Running 7zip compress command"
@@ -19,3 +21,16 @@ echo "Running 7zip compress command"
 rm -Rv /home/minecraft/papernut_"$(date +%F)"
 
 scp -l 7600 /home/minecraft/Backups/papernut_"$(date +%F)".7z jesse@desktop-vm:~/GDrive/Minecraft_Backups
+
+main_file=$(stat -c %s /home/minecraft/Backups/papernut_"$(date +%F)".7z)
+remote_file=$(ssh jesse@desktop-vm stat -c %s /home/jesse/GDrive/Minecraft_Backups/papernut_"$(date +%F)".7z)
+if [[ "$main_file" -ne "$remote_file" ]]; then
+  scp -l 7600 /home/minecraft/Backups/papernut_"$(date +%F)".7z jesse@desktop-vm:~/GDrive/Minecraft_Backups
+  if [[ "$main_file" -ne "$remote_file" ]]; then
+    ssh jesse@desktop-vm sudo reboot
+    sleep 300
+    scp -l 7600 /home/minecraft/Backups/papernut_"$(date +%F)".7z jesse@desktop-vm:~/GDrive/Minecraft_Backups
+  fi
+else
+  echo "Files are fine."
+fi

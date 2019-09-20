0e68cabc8fc4785796eebf74858f07c54ccac827
Jesse N. Richardson <jr.fire.flare@gmail.com>
2019-09-20 00:25:13 -0400

    Fixed the File Uploader to REQUIRE root

--- fileuploader.sh
+++ fileuploader.sh
@@ -2,7 +2,11 @@
 # Rat's Script to turn off and on the uploader
 # Written by Jesse N. Richardson (negativeflare@negativeflare.xyz) [negativeflare]
 
-
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   echo "Run this with sudo bash <filename>"
+   exit 1
+fi
 
 
 if [ $1 = on ]; then

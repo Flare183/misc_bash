0833b9121df17aad49e564a462156ea1850e1d8f
Jesse N. Richardson <jr.fire.flare@gmail.com>
2020-03-09 12:15:02 -0400

    Added the SSD Stage script Wrote a Russian Block Script

--- ssdstage.sh
+++ ssdstage.sh
@@ -4,4 +4,9 @@
 # First off Check to see if there is data on SSD
 backups=$(ls -A /mnt/SSD/Staging/Backups)
 
+
 if [ "$backups" ]; then
+  cp -Rv /mnt/SSD/Staging/Backups/* /mnt/Backups/
+else
+  echo "Directory is empty, quitting the backups part"
+fi

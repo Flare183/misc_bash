40e88f4918eb26c632b1a9d29ffce7b2e8bdf0d5
Jesse Richardson <flare183@charter.net>
2020-06-19 23:40:27 -0400

    Updated the Shop Announcer Script

--- shop_announce.sh
+++ shop_announce.sh
@@ -3,4 +3,4 @@
 # Written by Jesse N. Richardson (jr.fire.flare@gmail.com) [negativeflare]
 
 
-tmux send-keys -t papernut broadcast\ Like\ our\ server?\ Don't\ forget\ to\ vote!\ Want\ to\ upgrade\ your\ survival\ experience?\ Check\ out\ our\ available\ ranks\ at\ our\ webstore\ /craftingstore:shop\ C-m
+tmux send-keys -t papernut broadcast\ Like\ our\ server?\ Don't\ forget\ to\ vote!\ Want\ to\ upgrade\ your\ survival\ experience?\ Check\ out\ our\ available\ ranks\ at\ our\ webstore\ /craftingstore:shop C-m

d4f1a5d35c68196b5a7c378d28286c3bb82fb762
Jesse Richardson <flare183@charter.net>
2021-07-06 18:47:02 -0400

    Updated Minecraft backup script, and added some files when I help aaron

---
+++ aaron.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Aaron's Test Program
+# Writte by Jesse N. Richardson with Aaron's Help
+
+echo "Today's Time is: "
+date
+
+echo "---------"
+echo "The Current System Update is: "
+uptime
+#ping -c 10 10.0.0.1 > ping-time
+#cat ping-time | grep time
+
+
+echo "------"
+
+echo "What's Your Name?"
+read name
+
+
+echo "Your name is: "
+echo $name
+
+#source derp.sh
--- minecraft_backup_typhlosion.sh
+++ minecraft_backup_typhlosion.sh
@@ -2,6 +2,11 @@
 # Minecraft Backup Script
 # Written by Jesse N. Richardson (negativeflare@negativeflare.xyz) [negativeflare]
 
+
+echo "This script is DEPRACATED DO NOT USE IT!"
+sleep 120
+
+
 echo "This is a Minecraft Backup Script, it looks for the Minecraft tmux session"
 echo "Then it looks for the 'Resonant Rise' folder, and backs it up"
 echo "Then 7zips it up and puts it in the backups folder"
@@ -54,7 +59,7 @@ sleep 50
 sync
 
 mkdir /mnt/cyndaquil/Minecraft_Compress/rr-typhlosion_"$(date +%F)"/
-cp -Rv /mnt/cyndaquil/ResonantRise/ /mnt/cyndaquil/Minecraft_Compress/rr-typhlosion_"$(date +%F)"/
+rsync -av --progress --max-size=10G /mnt/cyndaquil/ResonantRise/ /mnt/cyndaquil/Minecraft_Compress/rr-typhlosion_"$(date +%F)"/
 #cp -Rv /home/minecraft/waterfall/ /mnt/cyndaquil/Minecraft_Compress/ftb-typhlosion_"$(date +%F)"/
 #cp -Rv /home/minecraft/skyworld/ /mnt/cyndaquil/Minecraft_Compress/ftb-typhlosion_"$(date +%F)"/
 #cp -Rv /home/minecraft/lobby/ /mnt/cyndaquil/Minecraft_Compress/ftb-typhlosion_"$(date +%F)"/
@@ -65,71 +70,74 @@ sleep 10
 echo "Running 7zip compress command"
 /usr/local/bin/7zip /mnt/cyndaquil/rr-typhlosion_"$(date +%F)".7z /mnt/cyndaquil/Minecraft_Compress/rr-typhlosion_"$(date +%F)"
 rm -Rv /mnt/cyndaquil/Minecraft_Compress/rr-typhlosion_"$(date +%F)"
+sync
+sleep 20
+
 
 # Start the Server backup
 cd /home/minecraft/ResonantRise/
-tmux new -d -s rr java -server -Xmx8196M -Xms8196M -XX:+UnlockExperimentalVMOptions -XX:ParallelGCThreads=16 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+AggressiveOpts -XX:+CMSIncrementalPacing -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
+tmux new -d -s rr /usr/lib/jvm/java-8-openjdk-amd64/bin/java -server -Xmx8196M -Xms8196M -XX:+UnlockExperimentalVMOptions -XX:ParallelGCThreads=16 -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+AggressiveOpts -XX:+CMSIncrementalPacing -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
 
-echo "Uploading File for the first time..."
-cp /mnt/cyndaquil/rr-typhlosion_"$(date +%F)".7z /home/minecraft/GDrive/
+# echo "Uploading File for the first time..."
+# rsync -av --progress /mnt/cyndaquil/rr-typhlosion_"$(date +%F)".7z /home/minecraft/GDrive/
 
-main_file=$(stat -c %s /home/minecraft/rr-typhlosion_"$(date +%F)".7z)
-remote_file=$(stat -c %s /home/minecraft/GDrive/rr-typhlosion_"$(date +%F)".7z)
+# main_file=$(stat -c %s /mnt/cyndaquil/rr-typhlosion_"$(date +%F)".7z)
+# remote_file=$(stat -c %s /home/minecraft/GDrive/rr-typhlosion_"$(date +%F)".7z)
 
-main_file_second_check=$(stat -c %s /home/minecraft/rr-typhlosion_"$(date +%F)".7z)
-remote_file_second_check=$(stat -c %s /home/minecraft/GDrive/rr-typhlosion_"$(date +%F)".7z)
+# main_file_second_check=$(stat -c %s /mnt/cyndaquil/rr-typhlosion_"$(date +%F)".7z)
+# remote_file_second_check=$(stat -c %s /home/minecraft/GDrive/rr-typhlosion_"$(date +%F)".7z)
 
-if [[ "$main_file" -ne "$remote_file" ]]; then
-  echo "Files Sizes aren't the same, Trying again..."
-  rm /home/minecraft/GDrive/rr-typhlosion_"$(date +%F)".7z
-  sleep 5
-  sync
-  cp /home/minecraft/rr-typhlosion_"$(date +%F)".7z /home/minecraft/GDrive/
-  sleep 10
-  if [[ "$main_file_second_check" -ne "$remote_file_second_check" ]]; then
-    echo "Files Sizes are STILL not the same, umounting and trying again."
-    sleep 100
-    fusermount -u /home/minecraft/GDrive
-    rclone mount --daemon GDrive:Minecraft_Backups/ /home/minecraft/GDrive/
-    sleep 10
-    ls /home/minecraft/GDrive/
-    rm /home/minecraft/GDrive/rr-typhlosion_"$(date +%F)".7z
-    sync
-    sleep 5
-    cp -v /home/minecraft/rr-typhlosion_"$(date +%F)".7z /home/minecraft/GDrive/
-  fi
-else
-  echo "File Sizes Are Fine"
-fi
+# if [[ "$main_file" -ne "$remote_file" ]]; then
+#   echo "Files Sizes aren't the same, Trying again..."
+#   rm /home/minecraft/GDrive/rr-typhlosion_"$(date +%F)".7z
+#   sleep 5
+#   sync
+#   cp /home/minecraft/rr-typhlosion_"$(date +%F)".7z /home/minecraft/GDrive/
+#   sleep 10
+#   if [[ "$main_file_second_check" -ne "$remote_file_second_check" ]]; then
+#     echo "Files Sizes are STILL not the same, umounting and trying again."
+#     sleep 100
+#     fusermount -u /home/minecraft/GDrive
+#     rclone mount --daemon GDrive:Minecraft_Backups/ /home/minecraft/GDrive/
+#     sleep 10
+#     ls /home/minecraft/GDrive/
+#     rm /home/minecraft/GDrive/rr-typhlosion_"$(date +%F)".7z
+#     sync
+#     sleep 5
+#     cp -v /mnt/cyndaquil/rr-typhlosion_"$(date +%F)".7z /home/minecraft/GDrive/
+#   fi
+# else
+#   echo "File Sizes Are Fine"
+# fi
 
-md5_main_file=$(md5sum --tag /home/minecraft/rr-typhlosion_"$(date +%F)".7z | cut -d '=' -f 2-)
-md5_remote_file=$(md5sum --tag /home/minecraft/GDrive/rr-typhlosion_"$(date +%F)".7z | cut -d '=' -f 2-)
+# md5_main_file=$(md5sum --tag /mnt/cyndaquil/rr-typhlosion_"$(date +%F)".7z | cut -d '=' -f 2-)
+# md5_remote_file=$(md5sum --tag /home/minecraft/GDrive/rr-typhlosion_"$(date +%F)".7z | cut -d '=' -f 2-)
 
-md5_main_file_second_check=$(md5sum --tag /home/minecraft/rr-typhlosion_"$(date +%F)".7z | cut -d '=' -f 2-)
-md5_remote_file_second_check=$(md5sum --tag /home/minecraft/GDrive/rr-typhlosion_"$(date +%F)".7z | cut -d '=' -f 2-)
+# md5_main_file_second_check=$(md5sum --tag /mnt/cyndaquil/rr-typhlosion_"$(date +%F)".7z | cut -d '=' -f 2-)
+# md5_remote_file_second_check=$(md5sum --tag /home/minecraft/GDrive/rr-typhlosion_"$(date +%F)".7z | cut -d '=' -f 2-)
 
-if [ "$md5_main_file" == "$md5_remote_file" ]; then
-  echo "File Hashes are fine."
-else
-    echo "Hashes aren't the same, trying again..."
-    rm /home/jesse/GDrive/rr-typhlosion_"$(date +%F)".7z
-    sleep 5
-    sync
-    cp -v /home/minecraft/rr-typhlosion_"$(date +%F)".7z /home/minecraft/GDrive/
-    sleep 10
-  if [ "$md5_main_file_second_check" == "$md5_remote_file_second_check" ]; then
-    echo "File Hashes are fine"
-    else
-      echo "Hashes are STILL not the same, umounting and trying again..."
-      sleep 100
-      fusermount -u /home/minecraft/GDrive/
-      rclone mount --daemon GDrive:Minecraft_Backups/ /home/minecraft/GDrive/
-      rm /home/jesse/GDrive/Minecraft_Backups/rr-typhlosion_"$(date +%F)".7z
-      sync
-      sleep 5
-      cp -v /home/minecraft/Backups/rr-typhlosion_"$(date +%F)".7z /home/minecraft/GDrive/
-  fi
-fi
+# if [ "$md5_main_file" == "$md5_remote_file" ]; then
+#   echo "File Hashes are fine."
+# else
+#     echo "Hashes aren't the same, trying again..."
+#     rm /home/jesse/GDrive/rr-typhlosion_"$(date +%F)".7z
+#     sleep 5
+#     sync
+#     cp -v /mnt/cyndaquil/rr-typhlosion_"$(date +%F)".7z /home/minecraft/GDrive/
+#     sleep 10
+#   if [ "$md5_main_file_second_check" == "$md5_remote_file_second_check" ]; then
+#     echo "File Hashes are fine"
+#     else
+#       echo "Hashes are STILL not the same, umounting and trying again..."
+#       sleep 100
+#       fusermount -u /home/minecraft/GDrive/
+#       rclone mount --daemon GDrive:Minecraft_Backups/ /home/minecraft/GDrive/
+#       rm /home/jesse/GDrive/Minecraft_Backups/rr-typhlosion_"$(date +%F)".7z
+#       sync
+#       sleep 5
+#       cp -v /mnt/cyndaquil/rr-typhlosion_"$(date +%F)".7z /home/minecraft/GDrive/
+#   fi
+# fi
 
 # # Start the Server backup
 # cd /home/minecraft/ResonantRise/
@@ -144,5 +152,5 @@ fi
 #tmux send-keys -t ftb mvconfirm C-m
 #rm -Rv /home/minecraft/ftb/resource_nether
 #tmux send-keys -t ftb mv create resource_nether nether C-m
-tmux send-keys -t rr say\ Backup\ Is\ Complete C-m
+# tmux send-keys -t rr say\ Backup\ Is\ Complete C-m
 echo "Done"

51558e0584cd76e2836579ace57358e3914b0b91
Jesse Richardson <flare183@charter.net>
2023-01-09 09:10:18 -0500

    Added gitignore Updated Minecraft backup script for typhlosion. Should work fine now. Just need to implement crash control.

--- minecraft_backup_typhlosion.sh
+++ minecraft_backup_typhlosion.sh
@@ -75,7 +75,7 @@ sleep 20
 
 # Start the Server backup
 cd /mnt/mudkip/crazycraft/
-exec /mnt/mudkip/crazycraft/start.sh
+tmux new -d -s crazycraft /usr/lib/jvm/java-8-openjdk-amd64/bin/java -Dlog4j2.formatMsgNoLookups=true -Xms12G -Xmx12G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar forge.jar nogui
 
 # echo "Uploading File for the first time..."
 # rsync -av --progress /mnt/cyndaquil/rr-typhlosion_"$(date +%F)".7z /home/minecraft/GDrive/

c6e79d657d70be447f270aacb3d4c6a0ef60cacf
Jesse Richardson <flare183@charter.net>
2023-02-07 09:23:02 -0500

    Started working on iolite minecraft update script

---
+++ iolite_minecraft_upload.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# iolite Minecraft Upload Script
+# Written by Jesse N. Richardson (jr.fire.flare@gmail.com) [negativeflare]
+# This script should be ran at around 3AM
+
+
+# Make sure the rclone mounts are working

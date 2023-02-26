82093ea9aa733e42c785d25ada1b0156d1435490
Jesse Richardson <flare183@charter.net>
2023-02-26 11:27:48 -0500

    Wrote a script that backs up both world and player data

---
+++ dailybackup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Forge Player and World Data Backup Daily Backup Script
+# Written by Jesse N. Richardson (jr.fire.flare@gmail.com) [negativeflare]
+
+# This needs to run every ~2 hours
+# This copies all Player and World Data over to Minecraft_Compress and Compresses it and throw it into CC_Backup just like the other script
+
+# Main Vars
+timestamp=$(date +"%T" | sed 's/:/_/g')
+
+echo "Backing up Player And World Data"
+echo "Informing Players"
+tmux send-keys -t crazycraft say\ Backing\ Up\ World\ and\ Player\ Data C-m
+tmux send-keys -t crazycraft say\ Hold\ On\ To\ Your\ Butts C-m
+sleep 2
+tmux send-keys -t crazycraft save-all C-m
+sleep 10
+sync
+
+# First Copy it
+# No point in using the player data alone
+#rsync -av --progress /mnt/mudkip/crazycraft/world/playerdata /mnt/cyndaquil/Minecraft_Compress/playerdata-$timestamp
+rsync -av --progress /mnt/mudkip/crazycraft/world /mnt/cyndaquil/Minecraft_Compress/worlddata-"$timestamp"
+sync
+
+# Then Compress it
+#tar cvzf /mnt/cyndaquil/CC_Backup/playerdata/playerdata-$timestamp.tar.gz /mnt/cyndaquil/Minecraft_Compress/playerdata-$timestamp
+tar cvf - /mnt/cyndaquil/Minecraft_Compress/worlddata-"$timestamp" | pigz -p 6 > /mnt/cyndaquil/CC_Backup/worlddata/worlddata-"$timestamp".tar.gz
+sleep 10
+sync
+
+# Now clean up
+echo "Cleaning up"
+rm -Rvf /mnt/cyndaquil/Minecraft_Compress/*

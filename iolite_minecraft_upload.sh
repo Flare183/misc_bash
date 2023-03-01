91e059499c88c30949e083b1c6369f1163a15361
Jesse Richardson <flare183@charter.net>
2023-03-01 12:22:33 -0500

    Added drive space checks to iolite mc script.

--- iolite_minecraft_upload.sh
+++ iolite_minecraft_upload.sh
@@ -7,13 +7,39 @@
 set -e
 
 # Make sure the rclone mounts are working
-if [[ -d "/home/minecraft/GDrive/Minecraft_Backups" ]]; then
-    echo "Google Drive Mounts Are Working"
+if [[ -d "/home/jesse/GDrive/Minecraft_Backups/" ]]; then
+    echo "Google Drive Mount is Working"
+else
+    echo "Google Drive Mount is not working"
+    break
 fi
+
 if [[ -d "/home/minecraft/OneDrive/Minecraft_Backups/" ]]; then
-    echo "OneDrive Mounts Are Working"
+    echo "OneDrive Mount is Working"
+else
+    echo "OneDrive Mount is not working"
+    break
+fi
+
+if [[ -d "/home/jesse/Mega/Minecraft_Backups/" ]]; then
+    echo "Mega Drive Mount is Working"
+else
+    echo "Mega Drive Mount is not working"
+    break
 fi
 
 echo "All Drive Mounts are working, let's continue."
+
+# Let's check for the required space on each drive, if there's enough space copy the file over.
+
+requiredspace=11264000
+gdrive_space=$(df /home/jesse/GDrive | awk 'NR==2 { print $4 }')
+onedrive_space=$(df /home/jesse/OneDrive | awk 'NR==2 { print $4 }')
+megadrive_space=$(df /home/jesse/Mega | awk 'NR==2 { print $4 }')
+
+
+
+if [[ ]]
+
 
 rsync -av --progress

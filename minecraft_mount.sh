05f8defcc48a58af0a1b267f7aebcfd76aa11738
Jesse Richardson <flare183@charter.net>
2023-03-06 09:56:26 -0500

    Moving Minecraft Backup Scripts to another repo

--- minecraft_mount.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Minecraft Mount Script
-# Written by Jesse N. Richardson (negativeflare@negativeflare.xyz) [negativeflare]
-
-sshfs jesse@iolite:/mnt/Backup/Minecraft_Backups/ Backups/
-rclone mount --daemon GDrive:Minecraft_Backups/ /home/minecraft/GDrive/
--- shop_announce.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Announcement Script For the Papernut Minecraft Server
-# Written by Jesse N. Richardson (jr.fire.flare@gmail.com) [negativeflare]
-
-
-tmux send-keys -t papernut "broadcast Like our server? Don't forget to vote! Want to upgrade your survival experience? Check out our available ranks at our webstore /craftingstore:shop" Enter
